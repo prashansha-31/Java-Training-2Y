99d0b8058293d0f0c67b1a7351a7579c7c8063a9
Prashansha Maheshwari <prashansha.maheshwari_cs.aim24@gla.ac.in>
2025-08-14 11:19:04 +0530

    [Prashansha Maheshwari]  RelationalOperators file

---
+++ RelationalOperators.java
@@ -0,0 +1,12 @@
+public class RelationalOperators {
+public static void main(String[] args) {
+int a = 10;
+int b = 5;
+System.out.println("a == b: " + (a == b)); // false
+System.out.println("a != b: " + (a != b)); // true
+System.out.println("a > b: " + (a > b)); // true
+System.out.println("a < b: " + (a < b)); // false
+System.out.println("a >= b: " + (a >= b)); // true
+System.out.println("a <= b: " + (a <= b)); // false
+}
+}
