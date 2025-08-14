e364b7ffdafba72c2b6511cd833de5586dce0159
Prashansha Maheshwari <prashansha.maheshwari_cs.aim24@gla.ac.in>
2025-08-14 11:25:47 +0530

    [Prashansha Maheshwari]  ArithmeticOperators file

---
+++ ArithmeticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Addition: " + (a + b));         // 13
+        System.out.println("Subtraction: " + (a - b));      // 7
+        System.out.println("Multiplication: " + (a * b));   // 30
+        System.out.println("Division: " + (a / b));         // 3
+        System.out.println("Modulus: " + (a % b));          // 1
+    }
+}
